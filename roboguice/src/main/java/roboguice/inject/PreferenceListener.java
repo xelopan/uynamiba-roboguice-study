b60a758bd0eb6d7dc7bf07f074754b91811e601f
mike <mike@localhost>
2011-04-19 14:04:44 -0400

    remove unused class

--- roboguice/src/main/java/roboguice/inject/PreferenceListener.java
+++ roboguice/src/main/java/roboguice/inject/PreferenceListener.java
@@ -21,6 +21,7 @@ import android.preference.PreferenceActivity;
 import com.google.inject.MembersInjector;
 import com.google.inject.TypeLiteral;
 import com.google.inject.spi.TypeEncounter;
+import com.google.inject.spi.TypeListener;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
@@ -30,7 +31,7 @@ import java.util.ArrayList;
  * 
  * @author Mike Burton
  */
-public class PreferenceListener implements StaticTypeListener {
+public class PreferenceListener implements TypeListener {
     protected Context context;
     protected ArrayList<PreferenceMembersInjector<?>> queue = new ArrayList<PreferenceMembersInjector<?>>();
 
@@ -48,21 +49,6 @@ public class PreferenceListener implements StaticTypeListener {
             }
             c = c.getSuperclass();
         }
-    }
-
-    @SuppressWarnings("unchecked")
-    public void requestStaticInjection(Class<?>... types) {
-        for (Class<?> c : types) {
-            while (c != null) {
-                for (Field field : c.getDeclaredFields()) {
-                    if (Modifier.isStatic(field.getModifiers()) && field.isAnnotationPresent(InjectPreference.class)) {
-                        new PreferenceMembersInjector(field, field.getAnnotation(InjectPreference.class)).injectMembers(null);
-                    }
-                }
-                c = c.getSuperclass();
-            }
-        }
-
     }
 
 
--- roboguice/src/main/java/roboguice/inject/ViewListener.java
+++ roboguice/src/main/java/roboguice/inject/ViewListener.java
@@ -21,6 +21,7 @@ import android.content.Context;
 import com.google.inject.MembersInjector;
 import com.google.inject.TypeLiteral;
 import com.google.inject.spi.TypeEncounter;
+import com.google.inject.spi.TypeListener;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
@@ -30,7 +31,7 @@ import java.util.ArrayList;
  * 
  * @author Mike Burton
  */
-public class ViewListener implements StaticTypeListener {
+public class ViewListener implements TypeListener {
     protected Context context;
     protected ArrayList<ViewMembersInjector<?>> queue = new ArrayList<ViewMembersInjector<?>>();
 
@@ -47,16 +48,6 @@ public class ViewListener implements StaticTypeListener {
 
     }
 
-    @SuppressWarnings("unchecked")
-    public void requestStaticInjection(Class<?>... types) {
-            
-        for (Class<?> c : types)
-            for( ; c!=Object.class; c=c.getSuperclass() )
-                for (Field field : c.getDeclaredFields())
-                    if (Modifier.isStatic(field.getModifiers()) && field.isAnnotationPresent(InjectView.class))
-                        new ViewMembersInjector(field, field.getAnnotation(InjectView.class)).injectMembers(null);
-
-    }
 
     public void injectViews() {
         for (int i = queue.size() - 1; i >= 0; --i)
