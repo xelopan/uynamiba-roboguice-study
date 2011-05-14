8b47ba4cdb67254dd3e67992457f23d80ee692ff
mike <mike@localhost>
2011-05-14 13:46:50 -0700

    Move some bindings from context to app

--- roboguice/src/main/java/roboguice/inject/AssetManagerProvider.java
+++ roboguice/src/main/java/roboguice/inject/AssetManagerProvider.java
@@ -1,15 +1,15 @@
 package roboguice.inject;
 
-import android.content.Context;
+import android.app.Application;
 import android.content.res.AssetManager;
 
 import com.google.inject.Inject;
 import com.google.inject.Provider;
 
 public class AssetManagerProvider implements Provider<AssetManager> {
-    @Inject protected Context context;
+    @Inject protected Application app;
 
     public AssetManager get() {
-        return context.getAssets();
+        return app.getAssets();
     }
 }
