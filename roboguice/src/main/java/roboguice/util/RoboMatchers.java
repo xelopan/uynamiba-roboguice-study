9a0fe74e18471a5dcd9ed19af04ffb1f6745df89
mike <mike@localhost>
2011-05-14 14:27:57 -0700

    Change matchers for views to only work on activity classes by default

--- roboguice/src/main/java/roboguice/util/RoboMatchers.java
+++ roboguice/src/main/java/roboguice/util/RoboMatchers.java
@@ -1,5 +1,7 @@
 package roboguice.util;
 
+import android.app.Activity;
+import android.app.Service;
 import android.content.Context;
 
 import com.google.inject.TypeLiteral;
@@ -8,10 +10,22 @@ import com.google.inject.matcher.Matcher;
 
 public class RoboMatchers {
     public static Matcher<? super TypeLiteral<?>> subclassesOfContext() {
+        return subclassesOf(Context.class);
+    }
+
+    public static Matcher<? super TypeLiteral<?>> subclassesOfActivity() {
+        return subclassesOf(Activity.class);
+    }
+
+    public static Matcher<? super TypeLiteral<?>> subclassesOfService() {
+        return subclassesOf(Service.class);
+    }
+
+    public static Matcher<? super TypeLiteral<?>> subclassesOf( final Class<?> clazz ) {
         return new AbstractMatcher<TypeLiteral<?>>() {
                     @Override
                     public boolean matches(TypeLiteral<?> typeLiteral) {
-                        return Context.class.isAssignableFrom(typeLiteral.getRawType());
+                        return clazz.isAssignableFrom(typeLiteral.getRawType());
                     }
                 };
     }
