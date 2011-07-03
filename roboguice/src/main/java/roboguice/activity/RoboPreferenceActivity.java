7abdbc369ab8b6e6ca4fc62586d30d784c36f1a8
mike <mike@localhost>
2011-07-02 22:49:56 -0700

    fix bug in PreferenceListener

---
+++ roboguice/src/main/java/roboguice/activity/RoboPreferenceActivity.java
@@ -0,0 +1,184 @@
+/*
+ * Copyright 2009 Michael Burton
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+package roboguice.activity;
+
+import roboguice.activity.event.*;
+import roboguice.application.RoboApplication;
+import roboguice.event.EventManager;
+import roboguice.inject.ContextScope;
+import roboguice.inject.InjectorProvider;
+
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceScreen;
+import android.view.View;
+import android.view.ViewGroup.LayoutParams;
+
+import com.google.inject.Injector;
+
+/**
+ * A {@link RoboPreferenceActivity} extends from {@link PreferenceActivity} to provide
+ * dynamic injection of collaborators, using Google Guice.<br />
+ *
+ * Note: there is currently a limitation that prevents you from using @InjectView if you use setContentView.
+ * http://code.google.com/p/roboguice/issues/detail?id=70
+ * 
+ * @see RoboActivity
+ * 
+ * @author Toly Pochkin
+ * @author Rodrigo Damazio
+ * @author Mike Burton
+ */
+public abstract class RoboPreferenceActivity extends PreferenceActivity implements InjectorProvider {
+    protected EventManager eventManager;
+    protected ContextScope scope;
+
+    /** {@inheritDoc } */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        final Injector injector = getInjector();
+        eventManager = injector.getInstance(EventManager.class);
+        scope = injector.getInstance(ContextScope.class);
+        scope.enter(this);
+        injector.injectMembers(this);
+        super.onCreate(savedInstanceState);
+        eventManager.fire(new OnCreateEvent(savedInstanceState));
+    }
+
+    @Override
+    public void setPreferenceScreen(PreferenceScreen preferenceScreen) {
+        super.setPreferenceScreen(preferenceScreen);
+        scope.injectPreferenceViews();
+    }
+
+    @Override
+    public void setContentView(int layoutResID) {
+        super.setContentView(layoutResID);
+        scope.injectViews();
+        eventManager.fire(new OnContentViewAvailableEvent());
+    }
+
+    @Override
+    public void setContentView(View view, LayoutParams params) {
+        super.setContentView(view, params);
+        scope.injectViews();
+        eventManager.fire(new OnContentViewAvailableEvent());
+    }
+
+    @Override
+    public void setContentView(View view) {
+        super.setContentView(view);
+        scope.injectViews();
+        eventManager.fire(new OnContentViewAvailableEvent());
+    }
+
+    @Override
+    public Object onRetainNonConfigurationInstance() {
+        return this;
+    }
+
+    @Override
+    protected void onRestart() {
+        scope.enter(this);
+        super.onRestart();
+        eventManager.fire(new OnRestartEvent());
+    }
+
+    @Override
+    protected void onStart() {
+        scope.enter(this);
+        super.onStart();
+        eventManager.fire(new OnStartEvent());
+    }
+
+    @Override
+    protected void onResume() {
+        scope.enter(this);
+        super.onResume();
+        eventManager.fire(new OnResumeEvent());
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        eventManager.fire(new OnPauseEvent());
+    }
+
+    @Override
+    protected void onNewIntent( Intent intent ) {
+        super.onNewIntent(intent);
+        scope.enter(this);
+        eventManager.fire(new OnNewIntentEvent());
+    }
+
+    @Override
+    protected void onStop() {
+        scope.enter(this);
+        try {
+            eventManager.fire(new OnStopEvent());
+        } finally {
+            scope.exit(this);
+            super.onStop();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        scope.enter(this);
+        try {
+            eventManager.fire(new OnDestroyEvent());
+        } finally {
+            eventManager.clear(this);
+            scope.exit(this);
+            scope.dispose(this);
+            super.onDestroy();
+        }
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        final Configuration currentConfig = getResources().getConfiguration();
+        super.onConfigurationChanged(newConfig);
+        eventManager.fire(new OnConfigurationChangedEvent(currentConfig, newConfig));
+    }
+
+    @Override
+    public void onContentChanged() {
+        super.onContentChanged();
+        eventManager.fire(new OnContentChangedEvent());
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        scope.enter(this);
+        try {
+            eventManager.fire(new OnActivityResultEvent(requestCode, resultCode, data));
+        } finally {
+            scope.exit(this);
+        }
+    }
+
+    /**
+     * @see roboguice.application.RoboApplication#getInjector()
+     */
+    @Override
+    public Injector getInjector() {
+        return ((RoboApplication) getApplication()).getInjector();
+    }
+}
---
+++ roboguice/src/main/java/roboguice/util/RoboAsyncTask.java
@@ -0,0 +1,62 @@
+package roboguice.util;
+
+import roboguice.inject.ContextScope;
+
+import android.content.Context;
+import android.os.Handler;
+
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+
+import java.util.concurrent.Executor;
+
+/**
+ * Allows injection to happen for tasks that execute in a background thread.
+ * 
+ * @param <ResultT>
+ */
+public abstract class RoboAsyncTask<ResultT> extends SafeAsyncTask<ResultT> {
+    @Inject static protected Provider<Context> contextProvider;
+    @Inject static protected Provider<ContextScope> scopeProvider;
+
+    protected RoboAsyncTask() {
+    }
+
+    protected RoboAsyncTask(Handler handler) {
+        super(handler);
+    }
+
+    protected RoboAsyncTask(Handler handler, Executor executor) {
+        super(handler, executor);
+    }
+
+    protected RoboAsyncTask(Executor executor) {
+        super(executor);
+    }
+
+    @Override
+    protected Task<ResultT> newTask() {
+        return new Task<ResultT>(this);
+    }
+
+    protected static class Task<ResultT> extends SafeAsyncTask.Task<ResultT> {
+        protected Context context;
+        protected ContextScope scope;
+
+        public Task(SafeAsyncTask parent) {
+            super(parent);
+            this.context = contextProvider.get();
+            this.scope = scopeProvider.get();
+        }
+
+        @Override
+        protected ResultT doCall() throws Exception {
+            try {
+                scope.enter(context);
+                return super.doCall();
+            } finally {
+                scope.exit(context);
+            }
+        }
+    }
+}
